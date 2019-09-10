41ea8c21f1af1128d70f6206ddeda1866c63b977
Shariq Ali Asadi <shariq.asadi@gmail.com>
2019-09-10 10:46:10 +0500

    Adds Redux, redux form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
-
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Header from './Header';
 import Home from './Home';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard';
+import reducers from '../reducers';
 
 const App = () => (
   <div>
-  <BrowserRouter>
-    <Header />
-    <Route path='/' exact component={Home} />
-    <Route path='/signup' exact component={SignUp} />
-    <Route path='/signin' exact component={SignIn} />
-    <Route path='/dashboard' exact component={Dashboard} />
-  </BrowserRouter>
+    <Provider store={createStore(reducers, {})}>
+      <BrowserRouter>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/signup' exact component={SignUp} />
+        <Route path='/signin' exact component={SignIn} />
+        <Route path='/dashboard' exact component={Dashboard} />
+      </BrowserRouter>
+    </Provider>
   </div>
 );
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import { reduxForm, Field } from 'redux-form';
 class SignUp extends React.Component {
   render() {
     return (
-      <div>This is a signup Component</div>
-    )
+      <div>
+        <form>
+          <fieldset>
+            <Field name='email' type='text' id='email' component='input' />
+          </fieldset>
+          <fieldset>
+            <Field
+              name='password'
+              type='password'
+              id='password'
+              component='input'
+            />
+          </fieldset>
+          <button type='submit'>Sign Up</button>
+        </form>
+      </div>
+    );
   }
-};
+}
 
-export default SignUp;
+export default reduxForm({ form: 'signup' })(SignUp);
